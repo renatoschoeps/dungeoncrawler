21480e783a67319e691c0405690a0639a488cb5b
renatoschoeps <r.schoeps.a@gmail.com>
2026-09-22 00:40:31 +0200

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DungeonCrawler"
+
+include(":app")
+include(":game-core")
+include(":game-data")
